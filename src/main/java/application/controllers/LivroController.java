f049d54ce07fed017ca1931f7dd238e18ea23400
Ricardo Santos <ricardosds@gmail.com>
2021-10-26 19:53:38 +0000

    config controller for delete

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,10 +1,12 @@
 package application.controllers;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import application.models.Livro;
 import application.repositories.LivroRepository;
